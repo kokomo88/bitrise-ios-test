2ce1772e5a1b1f983fb2a544da399e42762545ab
kz <wcpapir10@freemail.hu>
2015-06-22 11:58:43 +0200

    test project init

---
+++ testbr/testbr/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  testbr
+//
+//  Created by kz on 22/06/15.
+//  Copyright (c) 2015 kz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ testbr/testbr/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  testbr
+//
+//  Created by kz on 22/06/15.
+//  Copyright (c) 2015 kz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
